744310b5b420c4babb4bfe5de9df25ccb437c8a5
Aksh-007 <akshaybarapatre54@gmail.com>
2023-02-04 13:20:11 +0530

    added some data

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -24,6 +24,36 @@ const Home = () => {
       imgSrc: img2,
       id: "2",
     },
+    {
+      name: "Mi 11 Ultra 5G",
+      price: 69999,
+      imgSrc: "https://i02.appmifile.com/867_operator_in/23/04/2021/3849c44ac78d65621750a114811711f9.png?width=140&height=140"
+},
+{
+      name: "Mi 11X Pro 5G",
+      price:  39999,
+      imgSrc: "https://i02.appmifile.com/52_operator_in/04/03/2021/7db1136dfdc2a1e9fc639b56bc19f17f.png?width=140&height=140"
+},
+{
+      name: "Mi 11X 5G",
+      price:  29999,
+      imgSrc: "https://i02.appmifile.com/368_operator_in/23/04/2021/93d54b25b3efc3b6e87a9d73004de1c3.png?width=140&height=140"
+},
+{
+      name: "Mi 10T Pro",
+      price: 36999,
+      imgSrc: "https://i02.appmifile.com/406_operator_in/15/10/2020/4481bf3c3bb2ce53d9f4ac6ce1d7e312.png?width=140&height=140"
+},
+{
+      name: "Mi 10T",
+      price: "from ₹ 32,999",
+      imgSrc: "https://i02.appmifile.com/938_operator_in/15/10/2020/4e6b056c8984ad7031873ae9b5f564a5.png?width=140&height=140"
+},
+{
+      name: "Mi 10i",
+      price: "from ₹ 20,999",
+      imgSrc: "https://i02.appmifile.com/606_operator_in/05/01/2021/3de2b587f140a830fc732855a0a76035.png?width=140&height=140"
+}
   ];
 
   const dispatch = useDispatch()
